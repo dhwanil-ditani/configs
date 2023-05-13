6c423be98e66e7b0a75ca560feea410ca3d476da
Dhwanil Ditani <ditanidhwanil@gmail.com>
2023-05-14 00:25:44 +0530

    Add options.lua

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+vim.opt.number = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.splitbelow = false
+vim.opt.splitright = false
+vim.opt.undofile = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+
